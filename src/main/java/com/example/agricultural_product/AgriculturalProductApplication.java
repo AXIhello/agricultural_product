b74108d7f28de2cd75c8678a12ebe875be236dcf
AXI <3306778393@qq.com>
2025-09-12 15:15:04 +0800

    Initial commit

---
+++ src/main/java/com/example/agricultural_product/AgriculturalProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.agricultural_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalProductApplication.class, args);
+    }
+
+}
